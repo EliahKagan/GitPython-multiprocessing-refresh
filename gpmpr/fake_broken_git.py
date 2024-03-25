2d4d1dc28b2b7db41335b55034679c6d8ee463fe
Eliah Kagan <degeneracypressure@gmail.com>
2024-03-25 18:20:30 -0400

    Make fake git's fakeness more readily visible

--- gpmpr/fake_broken_git.py
+++ gpmpr/fake_broken_git.py
@@ -8,7 +8,7 @@ from typing import NoReturn
 
 def main() -> NoReturn:
     if sys.argv[1:] == ["version"]:
-        print("fake git 99.999.9")
+        print("FAKE git 99.999.9")
         sys.exit(0)
 
     print(f"error: fake git only supports 'git version'")
