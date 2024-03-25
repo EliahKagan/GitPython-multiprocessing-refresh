7939e1720e02194ce0ebb932714f82a43e0dbd96
Eliah Kagan <degeneracypressure@gmail.com>
2024-03-25 16:52:59 -0400

    Basic idea for demo script
    This doesn't work yet, because g.version is actually a lambda, and
    thus not (by defualt) picklable.

--- gpmpr/mp.py
+++ gpmpr/mp.py
@@ -2,13 +2,22 @@
 
 __all__ = ["main"]
 
-from concurrent.futures import ProcessPoolExecutor
+import concurrent.futures
 import multiprocessing
 
 import git
 
 
 def main() -> None:
-    git.refresh("/usr/bin/git")  # FIXME: Somehow avoid hard-coding this.
+    git.refresh("/usr/bin/git")  # TODO: Somehow avoid hard-coding this.
     g = git.Git()
-    print(g.version())
+    ver = g.version()
+    print(f"In parent process: {ver}")
+
+    for method in "fork", "spawn", "forkserver":
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=1,
+            mp_context=multiprocessing.get_context(method),
+        ) as executor:
+            ver = executor.submit(g.version).result()
+            print(f"In child process ({method}): {ver}")
